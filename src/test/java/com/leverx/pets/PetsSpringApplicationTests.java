99e69639684c5323619fbf25d95d49d8817f3b43
andrei.panas <panas.andreww@gmail.com>
2021-06-15 13:51:33 +0300

    init

---
+++ src/test/java/com/leverx/pets/PetsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leverx.pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
