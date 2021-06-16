aa2ac0afb1cf4a2aff479fa9c62311b706e1f6ea
andrei.panas <panas.andreww@gmail.com>
2021-06-16 12:52:19 +0300

    changing errors' messages

---
+++ src/main/java/com/leverx/pets/util/ExceptionMessageUtil.java
@@ -0,0 +1,14 @@
+package com.leverx.pets.util;
+
+public class ExceptionMessageUtil {
+
+    //Pattern
+    public static final String PERSON_ERROR_PATTERN = "Person with id [%d] not found";
+    public static final String PET_ERROR_PATTERN = "Pet with id [%d] not found";
+
+    //Message
+    public static final String ENTITY_EXCEPTION = "Entity exception";
+    public static final String URL_NOT_FOUND = "URL not found";
+    public static final String INVALID_FORMAT = "Invalid format";
+    public static final String INTERNAL_ERROR = "Internal error";
+}
